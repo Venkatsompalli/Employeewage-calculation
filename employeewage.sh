da810d3ea8dc22f0bd4e530fd86ab03d34506132
Venkatsompalli <sompallivenkataswamy1@gmail.com>
2021-06-26 13:42:13 +0530

    Salary for one day

--- employeewage.sh
+++ employeewage.sh
@@ -4,8 +4,13 @@ ispresent=1;
 randomcheck=$((RANDOM%2));
 
 if [ $ispresent -eq $randomcheck ];
+
 then
       echo Employee is present
+      emprateperhr=20;
+      emphrs=8;
+      salary=$(($emphrs*$emprateperhr));
 else
       echo Employee is absent
+      salary=0;
 fi
