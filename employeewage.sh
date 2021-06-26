8c2c1011ac3755d96efc2085a028e23cc9877e8c
Venkatsompalli <sompallivenkataswamy1@gmail.com>
2021-06-26 14:24:46 +0530

    For a month

--- employeewage.sh
+++ employeewage.sh
@@ -3,11 +3,16 @@
 ispresent=1;
 ispresenthalf=2;
 emprateperhr=20;
+workingdays=20;
+totalworkingdays=0;
+totalemphrs=0;
+for (( i=1 ; i<=20 ; i++  ))
+do
 randomcheck=$((RANDOM%3));
 
 case $randomcheck in
         $ispresent)
-         emphrs=8
+         emphrs=8;
          ;;
          $ispresenthalf)
          emphrs=4;
@@ -16,6 +21,9 @@ case $randomcheck in
          emphrs=0;
          ;;
 esac
+totalemphrs=$(($totalemphrs+$emphrs));
 
-Salary=$(($emprateperhr*$emphrs));
+done
+
+Salary=$(($emprateperhr*$totalemphrs));
 echo Salary:$Salary
